3f9a3e9222fe71e732b7bb070f893f177b00c66c
gjsantana <gjsantana@scopus.com.br>
2020-07-14 22:33:19 -0300

    Ajustes de boas praticas conforme solicitado na pep8.
    close #40

--- pypro/settings.py
+++ pypro/settings.py
@@ -79,7 +79,7 @@ INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
 
 if DEBUG:
     INSTALLED_APPS.append('debug_toolbar')
-    MIDDLEWARE.insert(0,'debug_toolbar.middleware.DebugToolbarMiddleware')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
