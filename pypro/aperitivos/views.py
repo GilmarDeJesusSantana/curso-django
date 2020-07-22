109556c0b985266e58cf945ce0cbe3526fdb1aa7
gjsantana <gjsantana@scopus.com.br>
2020-07-21 22:04:05 -0300

    Substituido o dicionario por atributos de classe. close #66

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 439772041},
-    {'slug': 'update', 'titulo': 'Atualização do Sistema Operacional', 'vimeo_id': 440417149},
+    Video('motivacao', 'Video Aperitivo: Motivação', 439772041),
+    Video('update', 'Atualização do Sistema Operacional', 440417149),
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
