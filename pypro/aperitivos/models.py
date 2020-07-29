9d36187d820543f711f55b1ee1451826674e4f17
gjsantana <gjsantana@scopus.com.br>
2020-07-28 21:31:28 -0300

    Criado testes de video e migrações
    close #79

--- pypro/aperitivos/models.py
+++ pypro/aperitivos/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.urls import reverse
 
@@ -6,6 +7,7 @@ class Video(models.Model):
     slug = models.CharField(max_length=32)
     titulo = models.CharField(max_length=32)
     vimeo_id = models.CharField(max_length=32)
+    creation = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('aperitivos:video', args=(self.slug,))
