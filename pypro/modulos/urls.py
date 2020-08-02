f9bf1d67f00b211f4c38f5668b988ea1b8a070f4
gjsantana <gjsantana@scopus.com.br>
2020-08-02 14:07:37 -0300

    Criado página de indice.html
    close #104

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -6,5 +6,6 @@ app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
     path('aula/<slug:slug>', views.aula, name='aula'),
+    path('', views.indice, name='indice'),
 
 ]
